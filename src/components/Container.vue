ea58ebfad07957da466e66bc1da03b78e0dfd5a5
hyojin <myogle@gmail.com>
2023-06-24 02:25:45 +0900

    init

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="data in PostData" :key="i">
+        <Post :data="data"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+export default {
+  name: 'Container',
+  components: {
+    Post : Post
+  },
+  props: {
+    PostData : Array
+  }
+}
+</script>
+
+<style>
+
+</style>
